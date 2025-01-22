6d7e03ba538080561be685bbd8096770d6ac6ec1
Ashwani Sherawat <144014428+AshwaniS92@users.noreply.github.com>
2025-01-22 19:55:50 +0100

    Update 2.7_Up4.py

--- 2.7_Up4.py
+++ 2.7_Up4.py
@@ -113,6 +113,7 @@ elif page == "Most popular stations":
     - Address low activity in underperforming regions.
     - Optimize station placement and marketing in key zones.
     """)
+    
 Insights:
 •	Expand infrastructure in high-demand areas.
 •	Address low activity in underperforming regions.
